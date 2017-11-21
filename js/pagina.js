a94b2244e97402ef61d48871c425bdfb6e33686f
joacomachine <joaco.garcia01@hotmail.com>
2017-11-21 08:51:56 -0300

    14/11/17

--- js/pagina.js
+++ js/pagina.js
@@ -20,16 +20,16 @@ function iniciarGoogle() {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        alert(user.displayName)
+        
+        document.getElementById("jhony").src=user.fotoURL;
+        alert(user.fotoURL)
         
         
     }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
         var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
     });
